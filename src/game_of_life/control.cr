6a7cfe45cbe94beb92a8c670801c9e31273305b5
franza <nico.franza@gmail.com>
2020-11-19 00:46:18 +0100

    formatting

--- src/game_of_life/control.cr
+++ src/game_of_life/control.cr
@@ -7,35 +7,35 @@ class GameOfLife::Control
     quit_ch = Channel(Nil).new
     controls_ch = Channel(String).new
 
-  control_loop = spawn do
-    loop do
-      str = STDIN.read_line
-      controls_ch.send str.not_nil!.strip
+    control_loop = spawn do
+      loop do
+        str = STDIN.read_line
+        controls_ch.send str.not_nil!.strip
+      end
     end
-  end
 
-  main_loop = spawn do
-    @render.call
+    main_loop = spawn do
+      @render.call
 
-    loop do
-      select
-      when key = controls_ch.receive
-        if key == "q"
-          quit_ch.send nil
-          break
-        elsif key == "i"
-          @render_speed -= 100.milliseconds if @render_speed > 100.milliseconds
-        elsif key == "d"
-          @render_speed += 100.milliseconds
-        elsif key == ""
+      loop do
+        select
+        when key = controls_ch.receive
+          if key == "q"
+            quit_ch.send nil
+            break
+          elsif key == "i"
+            @render_speed -= 100.milliseconds if @render_speed > 100.milliseconds
+          elsif key == "d"
+            @render_speed += 100.milliseconds
+          elsif key == ""
+            @render.call
+          end
+        when timeout(@render_speed)
           @render.call
         end
-      when timeout(@render_speed)
-        @render.call
-      end
-    end    
-  end
+      end    
+    end
 
-  quit_ch.receive
+    quit_ch.receive
   end
 end
